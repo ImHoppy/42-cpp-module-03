decddc418f4cf6c71354cd75025bddb9f51a62f6
Marius BRAETS <mbraets@student.42.fr>
2022-06-22 12:57:10 +0200

    Ex00
    Finish

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,24 +1,29 @@
 #include "ClapTrap.hpp"
 
-#ifdef DEBUG
-	#define LOG(A) std::cout << "\033[3m" << A << "\033[0m" << std::endl 
+
+#define LOG(A) std::cout << "\033[3m" << A << "\033[0m" << std::endl
+#define PRINT(A) std::cout << "\033[1m" << "ClapTrap " << this->_name << A << "\033[0m" << std::endl
+
+#ifdef DEBUG_LOG
+	#define DEBUG(A) LOG(A)
 #else
-	#define LOG(A)
+	#define DEBUG(A)
 #endif
 
-/*
+
+/*f
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
 ClapTrap::ClapTrap(): _name("CL4P-TP"), _hitPoints(10), _EnergyPoints(10), _AttackDamage(0)
 {
-	LOG(this->_name << " is born!");
+	LOG( this->_name << " is born!" );
 }
 
 ClapTrap::ClapTrap( const ClapTrap & src )
 {
 	*this = src;
-	LOG(this->_name << " gets cloned!");
+	LOG( this->_name << " gets cloned!" );
 }
 
 
@@ -28,10 +33,8 @@ ClapTrap::ClapTrap( const ClapTrap & src )
 
 ClapTrap::~ClapTrap()
 {
-	LOG(this->_name << " is dead!");
+	LOG( this->_name << " is dead!");
 }
-
-
 
 /*
 ** --------------------------------- OVERLOAD ---------------------------------
@@ -39,6 +42,7 @@ ClapTrap::~ClapTrap()
 
 ClapTrap &				ClapTrap::operator=( ClapTrap const & rhs )
 {
+	DEBUG("Copy assignation operator called.");
 	if ( this != &rhs )
 	{ 
 		this->_name = rhs._name;
@@ -55,26 +59,47 @@ ClapTrap &				ClapTrap::operator=( ClapTrap const & rhs )
 
 void	ClapTrap::attack(const std::string & target)
 {
-	if (this->_EnergyPoints == 0 || this->_hitPoints == 0)
-		return ;
-	std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_AttackDamage << " points of damage!" << std::endl;
-	this->_EnergyPoints--;
+	DEBUG("attack method called.");
+
+	if (this->_EnergyPoints == 0)
+		PRINT(" not enought energy points.");
+	else if (this->_hitPoints == 0)
+		PRINT("  cannot perform an action while dead.  ");
+	else {
+		PRINT(" attacks " << target << ", causing " << this->_AttackDamage << " points of damage!");
+		this->_EnergyPoints--;
+	}
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
+	DEBUG("takeDamage method called.");
+
+	if (this->_hitPoints == 0)
+	{
+		PRINT(" cannot perform an action while dead.");
+		return ;
+	}
 	if (amount < this->_hitPoints)
 		this->_hitPoints -= amount;
 	else
 		this->_hitPoints = 0;
+	PRINT(" loses " << amount << " hit points.");
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
+	DEBUG("beRepaired method called.");
+	
 	if (this->_EnergyPoints == 0)
-		return ;
-	this->_hitPoints += amount;
-	this->_EnergyPoints--;
+		PRINT(" does not have enough energy points to perform an action.");
+	else if (this->_hitPoints == 0)
+		PRINT(" cannot perform an action while dead.");
+	else {
+		this->_hitPoints += amount;
+		this->_EnergyPoints--;
+		PRINT(" gets healed by " << amount << " hit points.");
+	}
 }
 
 /*
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,23 @@
+#include "ClapTrap.hpp"
+
 int main()
 {
+	ClapTrap a;
+	ClapTrap c;
+	{
+		ClapTrap b(a);
+		b.takeDamage(100);
+		b.beRepaired(1);
+	}
+	c.beRepaired(1);
 
+	for (int i = 10; i; i--)
+	{
+		a.attack("Bob");
+		c.takeDamage(1);
+	}
+	a.attack("Bob");
+	c.beRepaired(1);
+	c.takeDamage(2);
+	c.takeDamage(1);
 }
