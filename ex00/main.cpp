e6309d5f9eee25963d578045d4f5d5becde4df39
Marius BRAETS <mbraets@student.42.fr>
2022-06-24 12:33:57 +0200

    Add more message in main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,10 @@
 #include "ClapTrap.hpp"
 
+#define MAIN_LOG(A) std::cout << "\033[3;35m" << A << "\033[0m" << std::endl
+
 int main()
 {
+	MAIN_LOG("Initialize.");
 	ClapTrap a;
 	ClapTrap c;
 	{
@@ -11,12 +14,15 @@ int main()
 	}
 	c.beRepaired(1);
 
+	MAIN_LOG("A loosing all energy and C loosing all hp.");
 	for (int i = 10; i; i--)
 	{
 		a.attack("Bob");
 		c.takeDamage(1);
 	}
+	MAIN_LOG("Test if A can still do action without energy");
 	a.attack("Bob");
+	MAIN_LOG("Test if C can still do action when dead");
 	c.beRepaired(1);
 	c.takeDamage(2);
 	c.takeDamage(1);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,14 @@
 #include "ScavTrap.hpp"
 
+#define MAIN_LOG(A) std::cout << "\033[3;35m" << A << "\033[0m" << std::endl
+
 int main()
 {
+	MAIN_LOG("Initialize");
 	ScavTrap a("Dinklebot");
 	ScavTrap c;
 	{
+		MAIN_LOG("Test inherit");
 		ScavTrap *d = new ScavTrap("Henry");
 		ClapTrap e = *d;
 		e.takeDamage(1);
@@ -16,11 +20,13 @@ int main()
 	}
 	c.beRepaired(1);
 
+	MAIN_LOG("A loosing 10 energy and C goind to die");
 	for (int i = 10; i; i--)
 	{
 		a.attack("Bob");
 		c.takeDamage(10);
 	}
+	MAIN_LOG("Test if C can still do action when dead");
 	a.attack("Bob");
 	c.beRepaired(1);
 	c.takeDamage(2);
